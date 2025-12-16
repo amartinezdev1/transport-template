08dc4031613de052dc643ecd524edaa3fb42378e
amartinezdev1 <angelmarti18@icloud.com>
2025-12-15 23:06:47 -0500

    add initial configuration files and update README for project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
+	},
+});
